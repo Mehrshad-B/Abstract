589dd3eb192e8930ed550792e3ff358e9e511a99
Mehrshad <Besarati>
2024-09-11 21:58:30 -0400

    fixing css for thefotter copyrith and adding all the footer columns wiht a little bit of css to them

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,35 @@ function Footer() {
   return (
     <footer className="footer-container">
       <div className="footer-column">
-        <h3>Abstract</h3>
+        <h3 className="footer-title">Abstract</h3>
         <a href="">Start Trial</a>
         <a href="">Pricing</a>
         <a href="">Download</a>
+      </div>
+      <div className="footer-column">
+        <h3 className="footer-title">Resources</h3>
+        <a href="">Blog</a>
+        <a href="">Help Center</a>
+        <a href="">Release Notes</a>
+        <a href="">Status</a>
+      </div>
+      <div className="footer-column">
+        <h3 className="footer-title">Community</h3>
+        <a href="">Twitter</a>
+        <a href="">LinkedIn</a>
+        <a href="">Facebook</a>
+        <a href="">Dribbble</a>
+        <a href="">Podcast</a>
+      </div>
+      <div className="footer-column">
+        <h3 className="footer-title">Company</h3>
+        <a href="">About Us</a>
+        <a href="">Careers</a>
+        <a href="" id="break">
+          Legal
+        </a>
+        <strong>Contact Us</strong>
+        <a href="mailto:info@abstract.com">info@abstract.com</a>
       </div>
       <div className="copyright-footer-container">
         <AbstractIcon />
